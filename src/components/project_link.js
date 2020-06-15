f1a6aa08d209522941b02db283966c5893b874a2
Clara <cg2984@nyu.edu>
2020-06-15 18:20:10 -0400

    set up the home page

---
+++ src/components/project_link.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProjectLink() {
+  return (
+    <div className = "project-link_home">
+      	<div className="project_txt">
+        	<a href = "lile"><h2>Lil' Einstein Website Project</h2></a>
+        	<p> A UX project to redesign a toy website</p>
+        	<img class = "project_pic" src="https://live.staticflickr.com/65535/49696081927_f6d02f2d83_o.png" alt="usertesting"/>
+      	</div>
+    </div>
+  );
+}
+
+export default ProjectLink;
