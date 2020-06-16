76ead2801ee90ae9ef5fb2e548a353b272e79f89
Clara <cg2984@nyu.edu>
2020-06-16 00:16:22 -0400

    Updates

---
+++ precache-manifest.e2949b23642c3fb8bba85d64c8efc372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c42ac7b26ed95e7ffa38b2bf82a9f4",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "1befc6d106f749af98ab",
+    "url": "/test-app/static/css/main.ddae4838.chunk.css"
+  },
+  {
+    "revision": "fd9e60ed3b491aada9bc",
+    "url": "/test-app/static/js/2.0851ef69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.0851ef69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1befc6d106f749af98ab",
+    "url": "/test-app/static/js/main.7c4bf13c.chunk.js"
+  },
+  {
+    "revision": "1c3ac668049c0d68a235",
+    "url": "/test-app/static/js/runtime-main.83806e2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
