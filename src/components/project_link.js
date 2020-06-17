203a279d34bd56f342d090443841a40e72f67f33
Clara <cg2984@nyu.edu>
2020-06-17 12:42:16 -0400

    mapped the data response to a new project link

--- src/components/project_link.js
+++ src/components/project_link.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function ProjectLink() {
+function ProjectLink({projectName, projectDescription}) {
   return (
     <div className = "project-link_home">
       	<div className="project_txt">
-        	<a href = "lile"><h2 className = "h2_link">Lil' Einstein Website Project</h2></a>
-        	<p> A UX project to redesign a toy website</p>
-        	<img class = "project_pic" src="https://live.staticflickr.com/65535/49696081927_f6d02f2d83_o.png" alt="usertesting"/>
+        	<a href = "lile"><h2 className = "h2_link">{projectName}</h2></a>
+        	<p>{projectDescription}</p>
+        	<img className = "project_pic" src="https://live.staticflickr.com/65535/49696081927_f6d02f2d83_o.png" alt="usertesting"/>
       	</div>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import About from "../components/about.js"
 import ProjectLink from "../components/project_link.js"
+import axios from 'axios';
 
 function Home() {
-  
+  const [data, setData] = useState([]);
+  //----------------------Getting the data from my backend
+  useEffect(() => {
+    axios.get('https://tranquil-brushlands-15503.herokuapp.com/')
+    .then(function (response) {
+      console.log(response);
+      setData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }, []);
+
   return (
     <div className="home">
       <About/>
       <section className = "projects_home">
-        <ProjectLink/>
-        <ProjectLink/>
-        <ProjectLink/>
-        <ProjectLink/>
-        <ProjectLink/>
-        <ProjectLink/>
-        <ProjectLink/>
+        {data.map((project, i) => (
+          <ProjectLink projectName = {project.Name} projectDescription = {project.Description}/>
+        ))}
       </section>      
     </div>
   );
