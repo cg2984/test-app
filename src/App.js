f993086d97b1cdf99a25f60be5a45fa7b55a66a3
Clara <cg2984@nyu.edu>
2020-06-17 00:13:35 -0400

    importing axios and airtable

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Home from "./pages/home.js"
 import Article from "./pages/article.js"
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <Article/>
-    </div>
-  );
+  	return (
+    	<div className="App">
+      		<Article/>
+    	</div>
+  	);
 }
 
 export default App;
