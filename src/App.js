93047da5d541bd7b39d82a1ba1adacd85b86c365
Clara <cg2984@nyu.edu>
2020-06-17 12:15:35 -0400

    adding css and got the API call

--- src/App.js
+++ src/App.js
@@ -4,11 +4,36 @@ import Article from "./pages/article.js"
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 function App() {
+	//----------------------Getting the data from my backend
+	axios.get('https://tranquil-brushlands-15503.herokuapp.com/')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+  //const idFromTitle = title.replace(/\s+/g,"-").toLowerCase();
+
   	return (
     	<div className="App">
-      		<Article/>
+    		<Router>
+    			<Route exact path="/:id">
+      				<Article/>
+      			</Route>
+      			<Route exact path="/">
+      				<Home/>
+      			</Route>
+      		</Router>
     	</div>
   	);
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import About from "../components/about.js"
 import ProjectLink from "../components/project_link.js"
 
 function Home() {
+  
   return (
     <div className="home">
       <About/>
