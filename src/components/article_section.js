df837ce49e1b296bc4ba4a596f3e645cb4945c32
Clara <cg2984@nyu.edu>
2020-07-07 00:22:56 -0400

    adding more styling

--- src/components/article_section.js
+++ src/components/article_section.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function ArticleSection({sectionText, sectionImageOne, sectionImageTwo, sectionHeader}) {
+function ArticleSection({sectionText, sectionImageOne, sectionImageTwo, sectionImageThree, sectionImageFour, sectionHeader}) {
   return (
     <section>
-        <p className = "article_text">{sectionText}</p>  
-        <img className = "article_image_2" src = {sectionImageTwo} width="1080" height="1080"/>
-        <img className = "article_image_1" src = {sectionImageOne} width="1080" height="1080"/>
+    	<div className = "image_wrapper">  
+        	<img className = "article_image_2" src = {sectionImageTwo} width="1080" height="1080"/>
+        	<img className = "article_image_1" src = {sectionImageOne} width="1080" height="1080"/>
+        	<div className = "sub_image_wrapper">
+        		<img className = "article_image_3" src = {sectionImageThree} width="1080" height="1080"/>
+        		<img className = "article_image_4" src = {sectionImageFour} width="1080" height="1080"/>
+        	</div>
+    	</div>
+        <p className = "article_text">{sectionText}</p>
     </section>
   );
 }
--- src/pages/article.js
+++ src/pages/article.js
@@ -1,5 +1,6 @@
 import React, {useEffect,useState} from 'react';
 import ArticleSection from "../components/article_section.js";
+import ProjectLink from "../components/project_link.js";
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 import {
@@ -43,26 +44,30 @@ function Article() {
   return (
     <div className="article">  
       <Button className = "about_button" Link="/" Name="Back"/>
-      <header className = "project_statement">
+      <header className = "project_header">
         <h1>{data.Name}</h1>
-        <p className = "overview">{data.Overview}</p>
-        <hr></hr>
+        <p>{data.Overview}</p>
       </header>
       <div className = "goals">
           <div className = "goal_text">
             <h1> Goals </h1>
             <h2>{data.Goal1}</h2>
             <h2>{data.Goal2}</h2>
-            <h2>{data.Goal2}</h2>
+            <h2>{data.Goal3}</h2>
           </div>
           <img className = "goal_img" src= "https://dl.airtable.com/.attachmentThumbnails/99b288869545808aa91226b722f1b365/7ce33f8d"/>
       </div>
-      <h1 className = "article_header">Sketching & Research</h1>
-      <ArticleSection sectionText = {data.Sketching_Research} sectionImageOne = {data.Sketch_Image_1} sectionImageTwo = {data.Sketch_Image_2}/>
-      <h1 className = "article_header">Prototyping</h1>
-      <ArticleSection sectionText = {data.Prototyping} sectionImageOne = {data.Prototyping_Image_1} sectionImageTwo = {data.Prototyping_Image_2}/>
-      <h1 className = "article_header">Finalizing</h1>
-      <ArticleSection sectionText = {data.Finishing_Touches} sectionImageOne = {data.Finishing_Image_1} sectionImageTwo = {data.Finishing_Image_2}/>
+      <h1 className = "section_header">Sketching & Research</h1>
+      <ArticleSection className = "article_section" sectionText = {data.Sketching_Research} sectionImageOne = {data.Sketch_Image_1} sectionImageTwo = {data.Sketch_Image_2} sectionImageThree = {data.Sketch_Image_1} sectionImageFour = {data.Sketch_Image_2}/>
+      <h1 className = "section_header">Prototyping</h1>
+      <ArticleSection className = "article_section" sectionText = {data.Sketching_Research} sectionImageOne = {data.Sketch_Image_1} sectionImageTwo = {data.Sketch_Image_2} sectionImageThree = {data.Sketch_Image_1} sectionImageFour = {data.Sketch_Image_2}/>
+      <h1 className = "section_header">Finalizing</h1>
+      <ArticleSection className = "article_section" sectionText = {data.Sketching_Research} sectionImageOne = {data.Sketch_Image_1} sectionImageTwo = {data.Sketch_Image_2} sectionImageThree = {data.Sketch_Image_1} sectionImageFour = {data.Sketch_Image_2}/>
+      <footer>
+        <ProjectLink projectName = "test" projectDescription = 'test' projectImage = "test" projectId = "test"/>
+        <ProjectLink projectName = "test" projectDescription = 'test' projectImage = "test" projectId = "test"/>
+        <ProjectLink projectName = "test" projectDescription = 'test' projectImage = "test" projectId = "test"/>
+      </footer>
     </div>
   );
 }
