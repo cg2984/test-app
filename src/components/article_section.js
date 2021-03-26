c139c53464cf5dffe47faa85d17c5e5b9e2a9845
Clara <cg2984@nyu.edu>
2021-03-26 00:19:56 -0400

    fixing article

--- src/components/article_section.js
+++ src/components/article_section.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import { SRLWrapper } from "simple-react-lightbox";
 
 class Section extends Component {
   render(props) {
-    return(
-      <section className = "articleSection"> 
-      	<span>
-	        <h2 className = "section_title">{this.props.header}</h2>
-	        <p>{this.props.body}</p>
-        </span>
-        <SRLWrapper>
-        	<img src = {this.props.image} alt = "the final product"/>
-        </SRLWrapper>
-      </section>
+    return( 
+      <span>
+	       <h2 className = "section_title">{this.props.header}</h2>
+	       <p>{this.props.body}</p>
+      </span>
     )
     
   }
--- src/pages/article.js
+++ src/pages/article.js
@@ -8,10 +8,46 @@ import "../article.css"
 import Header from "../components/header.js";
 import Link from "../components/link.js";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import { SRLWrapper } from "simple-react-lightbox";
+
 
 function Article() {
   const [data, setData] = useState([]);
   const [projects, setProjects] = useState([]);
+  const [activeStep, setActiveStep] = useState(0);
+  let sectionArray = [
+    <Section
+        id = "Research"
+        header = {"Research"}
+        body = {data.Section1}
+        image = {data.Sketch_Image_1}
+      />, 
+      <Section
+        id = "Evalulate"
+        header = {"Prototyping"}
+        body = {data.Section2}
+        image = {data.Prototyping_Image_1}
+      />,
+      <Section
+        id = "Finalizing"
+        header = {"Evalulate"}
+        body = {data.Section3}
+        image = {data.Prototyping_Image_2}
+      />,
+      <Section
+        header = {"Reflection"}
+        body = {data.Reflection}
+      />
+  ]
+  let tempStep = activeStep
+
+  //right now not using these but might do later
+  const [onNext, setOnNext] = useState(false);
+  const [onBack, setOnBack] = useState(false);
   let id = ""; 
   let UrlId = "";
   
@@ -19,8 +55,9 @@ function Article() {
   UrlId = useParams();
   id = UrlId.id;
   let project_data = "https://tranquil-brushlands-15503.herokuapp.com/project/" + id;
-  let next_projects = "https://tranquil-brushlands-15503.herokuapp.com"
-  
+  let next_projects = "https://tranquil-brushlands-15503.herokuapp.com";
+
+  console.log(data.Image_Dump);
     
   //getting the data for this project  
   useEffect(() => {
@@ -46,7 +83,8 @@ function Article() {
     });
   },[next_projects]);
   console.log("prototype", data.Link_Prototype);
-      
+  
+
   return (
     <div className="article"> 
       <Header
@@ -56,28 +94,31 @@ function Article() {
         name={<ArrowBackIcon/>}
         location="/" 
       />
-      <Section
-        id = "Research"
-        header = {"Research"}
-        body = {data.Section1}
-        image = {data.Sketch_Image_1}
+      <MobileStepper
+        className="stepper"
+        variant="progress"
+        steps={4}
+        position="static"
+        activeStep={activeStep}
+        backButton={
+          <Button size="small" onClick={()=> setActiveStep(tempStep-=1)} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+        nextButton={
+          <Button size="small" onClick={()=> setActiveStep(tempStep+=1)} disabled={activeStep === 3}>
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
       />
-      <Section
-        id = "Evalulate"
-        header = {"Prototyping"}
-        body = {data.Section2}
-        image = {data.Prototyping_Image_1}
-      />
-      <Section
-        id = "Finalizing"
-        header = {"Evalulate"}
-        body = {data.Section3}
-        image = {data.Prototyping_Image_2}
-      />
-      <Section
-        header = {"Reflection"}
-        body = {data.Reflection}
-      />
+      {sectionArray[activeStep]}
+      <SRLWrapper>
+        {data.Image_Dump.map((item, i) => (
+          <img src = {data.Image_Dump[i]}/>
+        ))}
+      </SRLWrapper>
       <footer className = "article_footer">
         <h2>See another project</h2>
         <div className = "next_projects">
